96aea3f1b24bca8f62a4a08faea13dd3d44d8914
Duncan Ramuhashi <duncanramohashi@gmail.com>
2024-08-16 12:57:26 +0200

    ii

--- src/Display.jsx
+++ src/Display.jsx
@@ -7,15 +7,15 @@ function  Display(){
      <body >
 
              <div className='flex flex-col items-center justify-center py-10 px-4'>
-  <div className='p-6 sm:p-8 space-y-6 sm:space-y-10 rounded-lg  shadow-md'>
-    <h1 className='text-4xl sm:text-6xl md:text-9xl font-medium text-black text-center'>DRONOTICZ</h1>
-    <h1 className='text-2xl sm:text-3xl md:text-4xl text-center text-black'>THE ERA OF DRONES</h1>
+                <div className='p-6 sm:p-8 space-y-6 sm:space-y-10 rounded-lg  shadow-md'>
+                   <h1 className='text-4xl sm:text-6xl md:text-9xl font-medium text-black text-center'>DRONOTICZ</h1>
+               <h1 className='text-2xl sm:text-3xl md:text-4xl text-center text-black'>THE ERA OF DRONES</h1>
 
-    <div className='flex items-center justify-center'>
-      <div className='w-1/4 sm:w-1/6 h-1  '></div>
-    </div>
-  </div>
-</div>
+                    <div className='flex items-center justify-center'>
+              <div className='w-1/4 sm:w-1/6 h-1  '></div>
+              </div>
+             </div>
+            </div>
 
      </body>
   );
